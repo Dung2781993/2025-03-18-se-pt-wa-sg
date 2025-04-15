7858342478a440c2b6729319551c9fcffed9f91c
JasonLe <Dung2781993@gmail.com>
2025-04-15 20:56:09 +1000

    Update module 2 exercises

---
+++ Module_2/10_04_2025/index.js
@@ -0,0 +1,10 @@
+let count = 0;
+
+function increaseFollowers() {
+  count++;
+  document.getElementById("followerCount").innerText = count;
+}
+
+
+
+// If we use bootrap, the maximum number of columns is 12.
